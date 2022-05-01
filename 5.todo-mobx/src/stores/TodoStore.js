bcfde380534a553cdc260c28c30320f3dcb3369b
Lee Joo Hyung <wngud985@gmail.com>
2022-05-01 18:06:06 +0900

    store added with actions

---
+++ 5.todo-mobx/src/stores/TodoStore.js
@@ -0,0 +1,23 @@
+import { observable, action, computed, makeObservable } from "mobx";
+
+class TodoStore {
+  constructor() {
+    makeObservable(this);
+  }
+  @observable
+  _todo = {};
+
+  get todo() {
+    return this._todo;
+  }
+
+  @action
+  setTodoProps(name, value) {
+    this._todo = {
+      ...this._todo,
+      [name]: value,
+    };
+  }
+}
+
+export default new TodoStore();
